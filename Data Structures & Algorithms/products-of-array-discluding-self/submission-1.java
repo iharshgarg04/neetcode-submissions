8ba0935d59e308b5b434db77f57dd6b8101fe42f
Harsh Garg <143173757+iharshgarg04@users.noreply.github.com>
2026-06-23 23:35:04 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int arr1[] = new int[n];
+        int arr2[] = new int[n];
+        int ans[] = new int[n];
+
+        arr1[0] = 1;
+        arr2[n-1] = 1;
+
+        for(int i=1;i<n;i++){
+            arr1[i] = nums[i-1] * arr1[i-1];
+            arr2[n-i-1] = nums[n-i] * arr2[n-i];
+        }
+
+        for(int i=0;i<n;i++){
+            ans[i] = arr1[i] * arr2[i];
+        }
+        return ans;
+    }
+}  
