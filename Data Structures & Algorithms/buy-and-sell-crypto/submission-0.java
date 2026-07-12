5a7f3878beffb758a3de93b934abbd493a33b111
Harsh Garg <143173757+iharshgarg04@users.noreply.github.com>
2026-07-12 23:34:51 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int ans = 0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i] < buy){
+                buy = prices[i];
+            }
+            ans = Math.max(prices[i] - buy, ans);
+        }
+        return ans;
+    }
+}
