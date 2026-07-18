c82dc232421b1c1d176def50bee7dcf45315e6f6
Harsh Garg <143173757+iharshgarg04@users.noreply.github.com>
2026-07-18 23:32:37 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length;
+        Arrays.sort(piles);
+        int low = 1;
+        int high = piles[n-1];
+        int min = Integer.MAX_VALUE;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(canEatAllBananas(mid, piles, h)){
+                min = Math.min(min, mid);
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        return min;
+    }
+    private boolean canEatAllBananas(int mid, int[] piles, int h){
+        for(int i=0;i<piles.length;i++){
+            h -= (int) Math.ceil((double)piles[i]/mid);
+        }
+        return h>=0;
+    }
+}
