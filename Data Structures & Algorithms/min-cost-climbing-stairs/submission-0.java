da806ff19ccba37eef723901c1cd229e8ae5bb75
Harsh Garg <143173757+iharshgarg04@users.noreply.github.com>
2026-09-12 21:05:43 +0530

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int startAtZero = solve(cost, 0, n, 0);
+        int startAtone = solve(cost, 1, n, 0);
+        return Math.min(startAtZero, startAtone);
+    }
+
+    private int solve(int []cost, int index, int len, int ans){
+        if(index == len) return 0;
+
+
+        int onestep = solve(cost, index+1, len, ans+cost[index]);
+
+        int twostep = Integer.MAX_VALUE;
+        if(index < len-1){
+            twostep = solve(cost, index+2, len, ans+cost[index]);
+        }
+         
+        return cost[index] + Math.min(onestep, twostep);
+    }
+}
