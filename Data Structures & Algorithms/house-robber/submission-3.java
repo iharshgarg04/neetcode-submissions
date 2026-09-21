d7a07176c0cea04904ea69d4d9a73531ef16867a
Harsh Garg <143173757+iharshgarg04@users.noreply.github.com>
2026-09-22 00:15:08 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int startFromEnd = solve(nums,n-1, 0);
+        int startFromSecondLast = solve(nums,n-2, 0);
+        return Math.max(startFromEnd, startFromSecondLast);
+    }
+
+    private int solve(int nums[], int index, int ans){
+        if(index < 0) return ans;
+        if(index == 0) return ans + nums[index];
+
+        int max = 0;
+        for(int i=index-2; i>=0; i--){
+            max = Math.max(max, solve(nums, i, ans + nums[index]));
+        }
+
+        ans = Math.max(max, ans);
+
+        return ans;
+    }
+}
